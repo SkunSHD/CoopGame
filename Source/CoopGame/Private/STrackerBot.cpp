acab4e819be81b744ee6c45a59f6f7a36a7365fc
den <den@DESKTOP-6SNMKIF>
2021-09-13 22:11:10 +0300

    Spawn bots at random locations using EQS

--- Source/CoopGame/Private/STrackerBot.cpp
+++ Source/CoopGame/Private/STrackerBot.cpp
@@ -150,7 +150,7 @@ FVector ASTrackerBot::FindNextPathPoint()
 
 	UNavigationPath* NavPath = UNavigationSystemV1::FindPathToActorSynchronously(this, GetActorLocation(), PlayerPawn);
 
-	if (NavPath->PathPoints.Num() > 1)
+	if (NavPath && NavPath->PathPoints.Num() > 1)
 	{
 		return NavPath->PathPoints[1];
 	}
